0b7722f874f1566371665b54b251a5aab295510a
Diego Mogollón <diego.mogollon.camacho@gmail.com>
2022-10-22 23:53:33 -0500

    CRUD creation for the "Sales" model

---
+++ Controllers/SalesController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PruebaTecnicaMasiv.Models;
+using PruebaTecnicaMasiv.Services;
+
+namespace PruebaTecnicaMasiv.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalesController : ControllerBase
+    {
+        public SaleService _saleService;
+        public SalesController(SaleService saleService)
+        {
+            _saleService = saleService;
+        }
+        [HttpGet]
+        public async Task<ActionResult> GetAllSales()
+        {
+            return Ok(await _saleService.GetAllSales());
+        }
+        [HttpGet("GetSaleDetail{id}")]
+        public async Task<ActionResult> GetSaleDetail(string id)
+        {
+            return Ok(await _saleService.GetSaleById(id));
+        }
+        [HttpPost("Create")]
+        public async Task<ActionResult> CreateSale([FromBody] Sales sales)
+        {
+            if (sales == null)
+                return BadRequest();
+            if (sales.NameConsole == string.Empty)
+            {
+                ModelState.AddModelError("Console", "The sale shouldn't be empty");
+            }
+            await _saleService.CreateSale(sales);
+            return Created("Created", true);
+        }
+        [HttpPut("UpdateSale{id}")]
+        public async Task<ActionResult> UpdateSale([FromBody] Sales sale, string id)
+        {
+            if (sale == null)
+                return BadRequest();
+            if (sale.NameConsole == string.Empty)
+            {
+                ModelState.AddModelError("Console", "The Sale shouldn't be empty");
+            }
+            sale.Id = id;
+            await _saleService.UpdateSale(sale.Id, sale);
+            return Ok();
+        }
+        [HttpDelete("DeleteSale{id}")]
+        public async Task<ActionResult> DeleteSale(string id)
+        {
+            await _saleService.DeleteSale(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Services/SaleService.cs
@@ -0,0 +1,40 @@
+﻿using MongoDB.Driver;
+using PruebaTecnicaMasiv.Models;
+
+namespace PruebaTecnicaMasiv.Services
+{
+    public class SaleService
+    {
+        private readonly IMongoCollection<Sales> _sales;
+        public SaleService(IDbSettings settings)
+        {
+            var client = new MongoClient(settings.Server);
+            var database = client.GetDatabase(settings.Database);
+            _sales = database.GetCollection<Sales>("saleClient");
+        }
+        public async Task<List<Sales>> GetAllSales()
+        {
+            return await _sales.FindAsync(d => true).Result.ToListAsync();
+        }
+        public async Task<List<Sales>> GetSaleById(string id)
+        {
+            return await _sales.FindAsync(d => d.Id == id).Result.ToListAsync();
+        }
+        public async Task<List<Sales>> GetSaleByName(string nameConsole)
+        {
+            return await _sales.FindAsync(d => d.NameConsole == nameConsole).Result.ToListAsync();
+        }
+        public async Task CreateSale(Sales sale)
+        {            
+            await _sales.InsertOneAsync(sale);
+        }
+        public async Task UpdateSale(string id, Sales sale)
+        {
+            await _sales.ReplaceOneAsync(sale => sale.Id == id, sale);
+        }
+        public async Task DeleteSale(string id)
+        {
+            await _sales.DeleteOneAsync(d => d.Id == id);
+        }
+    }
+}
